9cafbb667d9d02169c10c3f6fef76b1e861d8bc9
changlin <542177932@qq.com>
2017-10-18 10:21:46 +0800

    v1.1.0

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var warning =function( condition){return condition};
 if (process.env.NODE_ENV !== 'production') {
     warning = function(condition, message) {
         if (condition && typeof console !== 'undefined') {
-            console.error(new Error(message));
+            if(typeof message==='string'){
+                message=new Error(message)
+            }
+            console.error(message)
         }
         return condition
     }
