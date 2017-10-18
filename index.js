4f1367cc8d160eb85888e0b9a25bb1a69504361c
changlin <542177932@qq.com>
2017-10-18 10:28:22 +0800

    v1.1.1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var warning =function( condition){return condition};
 if (process.env.NODE_ENV !== 'production') {
     warning = function(condition, message) {
-        if (condition && typeof console !== 'undefined') {
+        if (condition && typeof console === 'object'&&typeof console.error ==='function') {
             if(typeof message==='string'){
                 message=new Error(message)
             }
